50b1d571f844a36639fa85798048c6f9215e1873
nel <ochiifvr@gmail.com>
2026-06-13 09:45:18 +0300

    fix: real M-Pesa STK Push in cart, fix import json in events views

--- events/views.py
+++ events/views.py
@@ -1,3 +1,5 @@
+import json
+import dateutil.parser
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse, HttpResponse, HttpResponseForbidden
 from django.shortcuts import render
